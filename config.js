54001904accb0df28578a1c5c8d908f4801772b6
josantoboy-debug <josantoboy@gmail.com>
2026-08-29 13:52:46 -0500

    Add production Supabase client configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = Object.freeze({
+  appName: 'match-equipos',
+  appVersion: '1.0.0',
+  supabaseUrl: 'https://yvlayxmhcngdqribcmkh.supabase.co',
+  supabasePublishableKey: 'sb_publishable_9XPD5yoq2N1CQCnYt3D7Fg_IGQrZbJN',
+  operatorApiUrl: 'https://yvlayxmhcngdqribcmkh.supabase.co/functions/v1/operator-api',
+  matchApiUrl: 'https://yvlayxmhcngdqribcmkh.supabase.co/functions/v1/match-api'
+});
